529af62a980b91b91ac58e4e401e19bc75fb47ca
michael <michaeledavi12@gmail.com>
2023-04-05 11:45:24 -0300

    feat: popup window

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/profile.module.scss";
 const logo = require("../assets/images/devchallenges.png");
 const avatar = require("../assets/images/profile-pic-test.jpg");
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import { MdOutlineArrowDropUp } from "react-icons/md";
+import { MdPersonPin } from "react-icons/md";
+import { BsPeopleFill } from "react-icons/bs";
+import { TbLogout } from "react-icons/tb";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [dropdown, setDropdown] = useState(false);
+
+  const openPopup = () => {
+    setDropdown(!dropdown);
+  };
+
   return (
     <>
       <header className={styles.containerheader}>
@@ -17,7 +29,36 @@ export default function Home() {
         <div className={styles.avatar}>
           <Image src={avatar} alt="avatar" className={styles.avatarpicture} />
           <strong>Xanthe Neal</strong>
-          <MdOutlineArrowDropDown className={styles.arrow} />
+
+          <button onClick={openPopup}>
+            {dropdown ? (
+              <MdOutlineArrowDropUp className={styles.arrow} />
+            ) : (
+              <MdOutlineArrowDropDown className={styles.arrow} />
+            )}
+          </button>
+
+          {dropdown && (
+            <>
+              <div className={styles.popup}>
+                <p className={styles.popupone}>
+                  <MdPersonPin className={styles.popupicon} />
+                  My Profile
+                </p>
+                <p className={styles.popuptwo}>
+                  <BsPeopleFill className={styles.popupicon} />
+                  Group Chat
+                </p>
+
+                <div className={styles.logout}>
+                  <button>
+                    <TbLogout className={styles.popupicon} />
+                    Logout
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </header>
 
@@ -27,46 +68,50 @@ export default function Home() {
       </nav>
 
       <section className={styles.card}>
-        <div className={styles.profile}>
-          <div className={styles.profiletitle}>
-            <h6>profile</h6>
-            <p>Some info may be visible to other people</p>
+        <div className={styles.containercard}>
+          <div className={styles.profile}>
+            <div className={styles.profiletitle}>
+              <h6>profile</h6>
+              <p>Some info may be visible to other people</p>
+            </div>
+            <div className={styles.buttonedit}>
+              <Link href="/edit" className={styles.link_edit}>
+                Edit
+              </Link>
+            </div>
           </div>
-          <div className={styles.buttonedit}>
-            <button>Edit</button>
+
+          <div className={styles.photo}>
+            <p>Photo</p>
+            <Image src={avatar} alt="avatar" className={styles.avatarpicture} />
           </div>
-        </div>
 
-        <div className={styles.photo}>
-          <p>Photo</p>
-          <Image src={avatar} alt="avatar" className={styles.avatarpicture} />
-        </div>
+          <div className={styles.info}>
+            <p>Name</p>
+            <strong>Xanthe Neal</strong>
+          </div>
 
-        <div className={styles.info}>
-          <p>Name</p>
-          <strong>Xanthe Neal</strong>
-        </div>
+          <div className={styles.info}>
+            <p>Bio</p>
+            <strong>
+              I am a software developer and a big fan of devchallenges...
+            </strong>
+          </div>
 
-        <div className={styles.info}>
-          <p>Bio</p>
-          <strong>
-            I am a software developer and a big fan of devchallenges...
-          </strong>
-        </div>
+          <div className={styles.info}>
+            <p>Phone</p>
+            <strong>40028922</strong>
+          </div>
 
-        <div className={styles.info}>
-          <p>Phone</p>
-          <strong>40028922</strong>
-        </div>
+          <div className={styles.info}>
+            <p>Email</p>
+            <strong>michaeledavi12@gmail.com</strong>
+          </div>
 
-        <div className={styles.info}>
-          <p>Email</p>
-          <strong>michaeledavi12@gmail.com</strong>
-        </div>
-
-        <div className={styles.password}>
-          <p>password</p>
-          <strong>**************</strong>
+          <div className={styles.password}>
+            <p>password</p>
+            <strong>**************</strong>
+          </div>
         </div>
       </section>
     </>
